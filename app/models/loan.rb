775062cb62aed6670a69298769ad351e7228338b
Jared Harbison <jared.harbison@gmail.com>
2025-06-17 09:41:00 -0400

    feat(loan): add validations for lender amount limitations and credit score validation

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -11,4 +11,26 @@ class Loan < ApplicationRecord
   }
 
   validates :amount, numericality: { greater_than: 0 }
+  validate :amount_within_lender_limits
+  validate :client_meets_credit_score
+
+  private
+
+  def amount_within_lender_limits
+    return if lender.nil? || amount.nil?
+
+    if amount < lender.minimum_loan_amount
+      errors.add(:amount, "must be at least #{lender.minimum_loan_amount}")
+    elsif amount > lender.maximum_loan_amount
+      errors.add(:amount, "must be less than or equal to #{lender.maximum_loan_amount}")
+    end
+  end
+
+  def client_meets_credit_score
+    return if client.nil? || lender.nil? || client.credit_score.nil?
+
+    if client.credit_score < lender.minimum_credit_score
+      errors.add(:client, "does not meet the lender’s minimum credit score of #{lender.minimum_credit_score}")
+    end
+  end
 end
