f1290476c1e5baa20fea94d8bedd91a76376f3c8
Jared Harbison <jared.harbison@gmail.com>
2025-06-16 22:56:18 -0400

    feat: abstract na helper from clients to application, create additional na helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def display_or_na(value)
+    value.present? ? value : "N/A"
+  end
+
+  def display_currency_or_na(amount)
+    amount.present? ? number_to_currency(amount) : "N/A"
+  end
+
+  def display_percentage_or_na(value)
+    value.present? ? "#{number_with_precision(value, precision: 2)}%" : "N/A"
+  end
 end
--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,5 +1,2 @@
 module ClientsHelper
-  def display_or_na(value)
-    value.present? ? value : "N/A"
-  end
 end
